a5f13a2a07d3be5bc90524afecd999ce03a9975b
Anna Borodaienko <anna.borodayenko@gmail.com>
2023-01-09 23:01:15 +0200

    Main section, forecast section

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View, Image } from 'react-native';
+import { Alert, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import * as Location from 'expo-location';
 import * as SplashScreen from 'expo-splash-screen';
 import axios from 'axios';
@@ -9,10 +9,12 @@ import { useFonts } from 'expo-font';
 SplashScreen.preventAutoHideAsync();
 
 export default function App () {
-  const [location, setLocation] = useState([]);
+  const [location, setLocation] = useState([50.58921087278374, 30.51444583685887]);
   const [address, getAddress] = useState([]);
   const [weather, getWeather] = useState([]);
+  const [hourlyForecast, getHourlyForecast] = useState([])
   const [iconPath, getIconPath] = useState('');
+  const [date, getDate] = useState('');
   const [fontsLoaded] = useFonts({
     'Ruluko': require('./assets/Ruluko-Regular.ttf'),
   });
@@ -36,9 +38,7 @@ export default function App () {
 
       let {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({});
       setLocation([latitude, longitude]);
-      console.log(latitude, longitude);
     })();
-    getCity(location);
   }, []);
 
   const getCity = async (location) => {
@@ -47,9 +47,14 @@ export default function App () {
   }
 
   const getWeatherData = async (location) => {
-    const {data} = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${location[0]}&lon=${location[1]}&exclude={part}&appid=${API_key_weather}&units=metric`)
-    getWeather([data.current.temp, data.current.weather[0].description]);
-    getIconPath(`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`);
+    const {data: {current}} = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${location[0]}&lon=${location[1]}&appid=${API_key_weather}&units=metric&exclude=minutely,alerts`)
+    console.log(current);
+    getWeather([current.temp, current.weather[0].main, current.humidity, current.wind_speed]);
+    // const {data: } = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${location[0]}&lon=${location[1]}&appid=${API_key_weather}&units=metric&exclude=current,minutely,alerts`)
+    // getHourlyForecast(daily);
+    getIconPath(`http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`);
+    // getDate((new Date(data.daily[0].dt*1000)).toLocaleDateString());
+    
   }
 
   useEffect(() => {
@@ -59,17 +64,84 @@ export default function App () {
 
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={styles.location}>{address.join(', ')}</Text>
-      <View style={styles.weather}>
-        <Text style={styles.temp}>Temp: {Math.round(weather[0])}</Text>
-        <Image
-          style={styles.icon}
-          source={{
-            uri: `${iconPath}`,
-          }}
-        />
-        <Text style={styles.description}>{weather[1]}</Text>
+      <View style={styles.user_info}>
+        <Text style={styles.location}>{address.join(', ')}</Text>
+        <Text style={styles.date}>09/01/23</Text>
       </View>
+      
+      <View style={styles.main}>
+        
+        <View style={styles.weather}>
+          <View style={styles.main_block}>
+            <Image
+              style={styles.icon}
+              source={{
+                uri: `${iconPath}`,
+              }}
+            />
+            <Text style={styles.temp}>{Math.round(weather[0])}°</Text>
+          </View>
+          
+          <Text style={styles.description}>{weather[1]}</Text>
+
+          <View style={styles.second_info}>
+            <Image
+              style={styles.small_icon}
+              source={{
+                uri: './assets/heweather-icon-S1-bw-source_399.png',
+              }}
+            />
+            <Text style={styles.info}>{weather[2]}%</Text>
+            <Image
+              style={styles.small_icon}
+              source={{
+                uri: `./assets/heweather-icon-S1-bw-source_507.png`,
+              }}
+            />
+            <Text style={styles.info}>{weather[3]}m</Text>
+          </View>
+          
+        </View>
+      </View>
+      
+      <ScrollView horizontal style={styles.forecast}>
+        <View style={styles.card}>
+          <Text style={styles.item}>1</Text>
+          <Text style={styles.item}>2</Text>
+          <Text style={styles.item}>3</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.item}>1</Text>
+          <Text style={styles.item}>2</Text>
+          <Text style={styles.item}>3</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.item}>1</Text>
+          <Text style={styles.item}>2</Text>
+          <Text style={styles.item}>3</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.item}>1</Text>
+          <Text style={styles.item}>2</Text>
+          <Text style={styles.item}>3</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.item}>1</Text>
+          <Text style={styles.item}>2</Text>
+          <Text style={styles.item}>3</Text>
+        </View>
+
+        <View style={styles.card}>
+          <Text style={styles.item}>1</Text>
+          <Text style={styles.item}>2</Text>
+          <Text style={styles.item}>3</Text>
+        </View>
+        
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -79,26 +151,81 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'rgba(39, 183, 245, 0.8)',
-    justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  user_info: {
+    flex: 1,
+    fontFamily: 'Ruluko',
   },
   location: {
     flex: 1,
     fontFamily: 'Ruluko',
-    marginTop: 40,
+    marginTop: 30,
+    fontSize: 40,
+    color: 'rgba(40, 36, 37, 0.9)'
   },
-  weather: {
+  date: {
+    flex: 1,
+    fontFamily: 'Ruluko',
+    fontSize: 20,
+    color: 'rgba(40, 36, 37, 0.9)',
+    textAlign: 'center'
+  },
+  main: {
+    flex: 4,
+    width: '100%',
+    height: 500,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  main_block: {
     flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  icon: {
+    width: 200,
+    height: 150,
   },
   temp: {
     fontFamily: 'Ruluko',
-  },
-  icon: {
-    width: 60,
-    height: 60,
+    fontSize: 40,
+    color: 'rgba(40, 36, 37, 0.9)'
   },
   description: {
     fontFamily: 'Ruluko',
-    textDecorationStyle: 'solid'
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  second_info: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  info: {
+    fontFamily: 'Ruluko',
+    fontSize: 20,
+    color: 'rgba(40, 36, 37, 0.9)'
+  },
+  small_icon: {
+    width: 40,
+    height: 40,
+    color: 'black'
+  },
+  forecast: {
+    borderColor: 'red',
+    borderWidth: 1
+  },
+  card: {
+    padding: 20,
+    margin: 20,
+    borderColor: 'red',
+    borderRadius: 5,
+    borderStyle: 'solid',
+    borderWidth: 1
+  },
+  item: {
+    fontSize: 20,
   }
 });
